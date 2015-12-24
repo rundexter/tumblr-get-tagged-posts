98f077cfc9d949039768369807f6760b5dc72a8c
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-24 13:41:05 +0200

    fix

--- index.js
+++ index.js
@@ -30,27 +30,6 @@ var pickInputs = {
     };
 
 module.exports = {
-    /**
-     * Return auth params.
-     *
-     * @param dexter
-     * @returns {*}
-     */
-    authOptions: function (dexter) {
-        var oauth = {
-            consumer_key: dexter.environment('tumblr_consumer_key'),
-            consumer_secret: dexter.environment('tumblr_consumer_secret'),
-            token: dexter.environment('tumblr_token'),
-            token_secret: dexter.environment('tumblr_token_secret')
-        };
-
-        return (
-            oauth.consumer_key &&
-            oauth.consumer_secret &&
-            oauth.token &&
-            oauth.token_secret
-        )? oauth : false;
-    },
 
     /**
      * The main entry point for the Dexter module
@@ -60,21 +39,15 @@ module.exports = {
      */
     run: function(step, dexter) {
         var inputs = util.pickStringInputs(step, pickInputs),
-            oauth = this.authOptions(dexter),
             uriLink = 'tagged';
-
-        if (!oauth)
-            return this.fail('A [tumblr_consumer_key,tumblr_consumer_secret,tumblr_token,tumblr_token_secret] environment need for this module.');
 
         if (!inputs.base_hostname || !inputs.api_key || !inputs.tag)
             return this.fail('A [base_hostname, api_key, tag] need for this module.');
 
-        inputs.api_key = oauth.consumer_key;
         //send API request
         request.get({
             url: uriLink,
             qs: _.omit(inputs, 'base-hostname'),
-            oauth: oauth,
             json: true
         }, function (error, response, body) {
             if (error)
